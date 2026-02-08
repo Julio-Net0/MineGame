b77dcc6dd375476894a813802fa299c2854283a6
Julio-Net0 <jjulio2468@gmail.com>
2026-02-08 10:37:41 -0300

    [SETUP-04]Language correction on BuildAndRun.ps1

--- BuildAndRun.ps1
+++ BuildAndRun.ps1
@@ -3,12 +3,12 @@ $EXECUTABLE = "MineGame.exe"
 
 cmake -S . -B $BUILD_DIR -G "Ninja" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
-Write-Host "`n--- Compilando com Ninja ---" -ForegroundColor Cyan
+Write-Host "`n--- Compiling with Ninja ---" -ForegroundColor Cyan
 cmake --build $BUILD_DIR
 
 if (Test-Path "$BUILD_DIR\$EXECUTABLE") {
-    Write-Host "`n--- Iniciando Jogo ---" -ForegroundColor Green
+    Write-Host "`n--- Starting game ---" -ForegroundColor Green
     & "$BUILD_DIR\$EXECUTABLE"
 } else {
-    Write-Host "`n[!] Erro: Executável não encontrado." -ForegroundColor Red
+    Write-Host "`n[!] Error: Executable not found." -ForegroundColor Red
 }
