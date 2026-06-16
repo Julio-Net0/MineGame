bb5f7c31c272e3620bf869cbff5c590a2c6381c2
Julio-Net0 <jjulio2468@gmail.com>
2026-06-16 11:35:56 -0300

    [FIX-15]Fixed integer division in floating point context for chat layout macros

--- include/chat.h
+++ include/chat.h
@@ -6,19 +6,22 @@
 #include "player.h"
 #include <stdbool.h>
 
+#define CHAT_Y_MARGIN_F ((float)GetScreenWidth() / 200.0F)
+#define CHAT_Y_MARGIN ((int)CHAT_Y_MARGIN_F)
+#define CHAT_HEIGHT_F ((float)GetScreenHeight() / 25.0F)
+#define CHAT_HEIGHT ((int)CHAT_HEIGHT_F)
+
 #define CHAT_WIDTH (GetScreenWidth() - (CHAT_Y_MARGIN * 2))
-#define CHAT_HEIGHT (GetScreenHeight() / 25)
-#define CHAT_X_MARGIN (GetScreenWidth() / 200)
-#define CHAT_Y_MARGIN (GetScreenWidth() / 200)
+#define CHAT_X_MARGIN CHAT_Y_MARGIN
 #define CHAT_POSITION_X CHAT_X_MARGIN
 #define CHAT_POSITION_Y (GetScreenHeight() - CHAT_HEIGHT - CHAT_Y_MARGIN)
 #define CHAT_MAX_INPUT_CHARS 256 // Recommended limit is 256 to prevent Stack Overflow on Windows (1MB stack)
 #define CHAT_MAX_HISTORY 256
 #define CHAT_DISPLAY_TIME 3.0F
 #define CHAT_FADE_TIME 1.0F
-#define CHAT_FONT_SIZE ((int)(CHAT_HEIGHT * 0.7F))
+#define CHAT_FONT_SIZE ((int)(CHAT_HEIGHT_F * 0.7F))
 #define CHAT_TEXT_PADDING 10
-#define CHAT_HISTORY_WIDTH ((GetScreenWidth() / 1.2) - CHAT_Y_MARGIN)
+#define CHAT_HISTORY_WIDTH ((GetScreenWidth() / 1.2) - CHAT_Y_MARGIN_F)
 #define CHAT_HISTORY_LIMIT_Y (GetScreenHeight() / 2)
 #define CHAT_BG_ALPHA 0.5F
 #define CHAT_HISTORY_BG_ALPHA 0.3F
